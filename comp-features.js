e1154d1baa6f3180efa08e57070848fe1c033b0c
Neopentene <82939252+Neopentene@users.noreply.github.com>
2022-12-31 22:44:00 +0530

    Delete comp-features.js

--- comp-features.js
+++
@@ -1,9 +0,0 @@
-function growTextArea(element, height) {
-    var remUnits = parseInt(getComputedStyle(document.documentElement).fontSize);
-    element.style.height = height + "rem";
-    element.style.height = element.scrollHeight - remUnits + "px";
-}
-function checkRadio(radio) {
-    radio.checked;
-    sessionStorage.setItem("filter", radio.value);
-}
